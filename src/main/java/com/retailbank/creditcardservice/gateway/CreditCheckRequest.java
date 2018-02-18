5a979dcf78729ec2305fd3b8cba0d10c68b3f315
amorgan <andrew.morgan@ing.nl>
2018-02-18 13:04:33 +0100

    m4-d2-regex

--- src/main/java/com/retailbank/creditcardservice/gateway/CreditCheckRequest.java
+++ src/main/java/com/retailbank/creditcardservice/gateway/CreditCheckRequest.java
@@ -1,7 +1,10 @@
 package com.retailbank.creditcardservice.gateway;
 
+import java.time.LocalDate;
+
 public class CreditCheckRequest {
     private final int citizenNumber;
+    private final String requestedDate = LocalDate.now().toString();
 
     public CreditCheckRequest(int citizenNumber) {
         this.citizenNumber = citizenNumber;
@@ -9,5 +12,9 @@ public class CreditCheckRequest {
 
     public int getCitizenNumber() {
         return citizenNumber;
+    }
+
+    public String getRequestedDate() {
+        return requestedDate;
     }
 }
